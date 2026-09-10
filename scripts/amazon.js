1fa1589c97ed62c0508e70a501884ab552c9c98b
myshamushataqzargar@gmail.com <mysha3298@gmail.com>
2026-09-10 10:14:20 +0530

    Make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -63,7 +63,7 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
           if(productId === item.productId){
             matchingItem = item;
           } 
-        })
+        });
         
         if (matchingItem){
           matchingItem.quantity += 1;
@@ -71,9 +71,15 @@ document.querySelectorAll('.js-add-to-cart').forEach(button => {
           cart.push({
             productId,
             quantity: 1
-          })
-        }
+          });
+        };
         
-    console.log(cart);
+        let cartQuantity = 0;
+        cart.forEach(item => {
+          cartQuantity += item.quantity;
+        });
+        
+        document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+        
     })
 })
