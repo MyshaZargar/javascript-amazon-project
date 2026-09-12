3528dc3a41eea84dd1aab988345dec820141a2eb
myshamushataqzargar@gmail.com <myshamushataqzargar@gmail.com>
2026-09-12 15:40:38 +0530

    Generate HTML for the order summary

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,11 @@
-export const cart = [];
+export const cart = [{
+    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+    quantity: 2
+},
+{
+    productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+    quantity: 3
+}];
 
 export function addToCart (productId) {
   //checks if the item is already present in the cart
