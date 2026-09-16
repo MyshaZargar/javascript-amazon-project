716688f5344458b3adc5107eb7c07a14698a5d18
myshamushataqzargar@gmail.com <myshamushataqzargar@gmail.com>
2026-09-16 19:40:22 +0530

    Save data and generate HTML for delivery options

--- data/cart.js
+++ data/cart.js
@@ -2,11 +2,13 @@ export let cart = JSON.parse(localStorage.getItem('cart'));
 if(!cart){
     cart = [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
+    quantity: 2,
+    deliveryOptionId : '1'
 },
 {
     productId: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
-    quantity: 3
+    quantity: 3,
+    deliveryOptionId: '2'
 }];
 }
 
@@ -30,7 +32,8 @@ export function addToCart (productId) {
   } else {
     cart.push({
       productId,
-      quantity: 1
+      quantity: 1,
+      deliveryOptionId: '1'
     });
   };
   saveToStorage();
@@ -46,7 +49,6 @@ export function removeFromCart(productId){
     cart = newCart;
     updateCartQuantity();
     saveToStorage();
-    
 }
 
 export function updateCartQuantity () {
